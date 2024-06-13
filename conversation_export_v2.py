f750652fc204d9551dfcfff0c3a2a6976623ce08
Saeed Siddik <saeed.siddik@iit.du.ac.bd>
2024-06-13 06:50:49 -0600

    Update conversation_export_v2.py

--- conversation_export_v2.py
+++ conversation_export_v2.py
@@ -9,3 +9,9 @@ import json
 with open('/Volumes/LaCie/ShareGPT/sg_90k_part1.json', 'r') as file:
     data = json.load(file)
 print(json.dumps(data[0], indent=4))
+
+# User: How many objects are there?
+file_path = '/Volumes/LaCie/ShareGPT/sg_90k_part1.json'
+with open(file_path, 'r') as file:
+    data = json.load(file)
+print(f'Total number of objects: {len(data)}')
