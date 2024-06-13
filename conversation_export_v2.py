b4a85f70f3bf59e38d1bcc27f64a5e7940d498ad
Saeed Siddik <saeed.siddik@iit.du.ac.bd>
2024-06-13 06:49:32 -0600

    Update conversation_export_v2.py

--- conversation_export_v2.py
+++ conversation_export_v2.py
@@ -3,3 +3,9 @@ import json
 with open('/Volumes/LaCie/ShareGPT/sg_90k_part1.json', 'r') as file:
     data = json.load(file)
 print(json.dumps(data[0], indent=4))
+
+# User: read sg_90k_part1.json and show the first object
+import json
+with open('/Volumes/LaCie/ShareGPT/sg_90k_part1.json', 'r') as file:
+    data = json.load(file)
+print(json.dumps(data[0], indent=4))
